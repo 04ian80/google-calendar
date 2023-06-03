060d46c0a99dbf61072b64e2689e9f1e3304d5f7
Ain Chung <artnouveau0804@gmail.com>
2023-06-03 22:48:29 +0900

    feat: set router for each page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import EventPostPage from './pages/EventPostPage';
+import EventDetailPage from './pages/EventDetailPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={process.env.BASE_URL}>
+      <Routes>
+        <Route path='/calendar' element={<Main />} />
+        <Route path='/events/new' element={<EventPostPage />} />
+        <Route path='/events/:eventId' element={<EventDetailPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/EventPostPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EventPostPage = () => {
+  return <div>EventPostPage</div>;
+};
+
+export default EventPostPage;
