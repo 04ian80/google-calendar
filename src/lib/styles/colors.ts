9b4683b740c6442fe01d5edee01f2e384f94ff0c
Ain Chung <artnouveau0804@gmail.com>
2023-06-08 05:39:44 +0900

    feat: create palette

---
+++ src/lib/styles/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  graySecondary: '#F1F3F4',
+  gray50: '#F6F6F6',
+  gray100: '#DADCE0',
+  gray200: '#5F6368',
+  gray800: '#3C4043',
+  mainBlue: '#4285F4',
+  mainGreen: '#34A853',
+  mainYellow: '#FBBC05',
+};
