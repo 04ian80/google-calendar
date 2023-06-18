27964681cca2e293ba5a73cf72d46cb88aee7410
Ain Chung <artnouveau0804@gmail.com>
2023-06-19 05:50:01 +0900

    feat: implements navigating to prev & next month

--- src/components/common/AdjacentArrows.tsx
+++ src/components/common/AdjacentArrows.tsx
@@ -1,15 +1,30 @@
 import styled from 'styled-components';
 
 import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
-import { colors } from '../../lib/styles/colors';
+import { colors } from '../../lib/styles/styles';
+import { GetMonthType } from '../../lib/types';
 
-const AdjacentArrows = () => {
+const AdjacentArrows = (props: GetMonthType) => {
   return (
     <Container>
-      <Button aria-label='이전 달로 이동하는 버튼'>
+      <Button
+        aria-label='이전 달로 이동하는 버튼'
+        onClick={() => {
+          if ('getPrevMonth' in props) {
+            props.getPrevMonth();
+          }
+        }}
+      >
         <RxCaretLeft size={25} />
       </Button>
-      <Button aria-label='다음 달로 이동하는 버튼'>
+      <Button
+        aria-label='다음 달로 이동하는 버튼'
+        onClick={() => {
+          if ('getNextMonth' in props) {
+            props.getNextMonth();
+          }
+        }}
+      >
         <RxCaretRight size={25} />
       </Button>
     </Container>
@@ -40,6 +55,10 @@ const Button = styled.button.attrs((props) => ({
 
   &:hover {
     background-color: ${colors.gray50};
+  }
+
+  &:active {
+    background-color: ${colors.gray100};
   }
 
   & > * {
