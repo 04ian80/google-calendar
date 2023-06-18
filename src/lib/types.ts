7084c85c72e054e03622304e2b49bb357b58c938
Ain Chung <artnouveau0804@gmail.com>
2023-06-19 05:48:03 +0900

    feat: declare useCalendar types
    각 컴포넌트에서 props로 전달되는 값이 각자 다 다르기 때문에 상황에 맞게 타입을 지정해주기 위해서 몇개는 따로 선언

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,22 @@
 import { EventHandler, MouseEvent } from 'react';
 
 export type MouseEventType<T = Element> = EventHandler<MouseEvent<T>>;
+
+type SelectedDType = {
+  selectedDate: number;
+};
+
+export type SelectedMYType = {
+  selectedMonth: number;
+  selectedYear: number;
+};
+
+export type SelectedFullDatesType = SelectedMYType & SelectedDType;
+
+export type GetMonthType = {
+  getCurrMonth?: () => void;
+  getPrevMonth: () => void;
+  getNextMonth: () => void;
+};
+
+export type CustomCalendarType = SelectedFullDatesType & GetMonthType;
